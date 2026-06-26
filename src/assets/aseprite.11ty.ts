0cb5b29bc46360e22e3d9da33e2f5f39229d3159
alex baldwin <ghostbaldwin@gmail.com>
2026-06-26 00:29:35 -0400

    portfolio tree template, about page, window animation, aseprite frames

--- src/assets/aseprite.11ty.ts
+++ src/assets/aseprite.11ty.ts
@@ -40,17 +40,6 @@ export default class {
     const file = await fs.readFile(entrypoint);
     const ase = Aseprite.parse(file, { clean: true, inflate: true });
     if (!ase.header) throw new Error(`missing header in ${entrypoint}`);
-    if (!ase.frames || ase.frames.length > 1) {
-      throw new Error(`wrong number of frames in ${entrypoint}`);
-    }
-    const frame = ase.frames[0];
-
-    const cel = frame.chunks.find((c) => c.type === ChunkTypeEnum.CEL);
-    if (!cel) throw new Error(`missing cel chunk in ${entrypoint}`);
-    if (cel.data.type !== CelTypeEnum.COMPRESSED)
-      throw new Error(
-        `unsupported cel type in ${entrypoint}: ${cel.data.type}`,
-      );
 
     const depth = ase.header?.pixelFormat;
     // https://github.com/aseprite/aseprite/blob/main/src/dio/aseprite_decoder.cpp#L60
@@ -61,46 +50,82 @@ export default class {
           ? ColorModeEnum.GRAYSCALE
           : ColorModeEnum.INDEXED;
 
-    let buffer: Buffer | Uint8Array = cel.data.pixels;
-    if (mode === ColorModeEnum.INDEXED) {
-      const palette = frame.chunks.find(
-        (c) => c.type === ChunkTypeEnum.PALETTE_OLD_1,
-      );
-      if (!palette) throw new Error(`missing palette chunk in ${entrypoint}`);
-      const originalBuffer = buffer;
-      buffer = new Uint8Array(originalBuffer.length * 4);
+    if (!ase.frames) throw new Error(`missing frames in ${entrypoint}`);
 
-      const transparentIndex = ase.header.transparentIndex;
-      const colors = palette.data.packets[0].colors;
+    const palette = ase.frames[0].chunks.find(
+      (c) => c.type === ChunkTypeEnum.PALETTE_OLD_1,
+    );
+    if (!palette)
+      throw new Error(`missing palette chunk in first frame of ${entrypoint}`);
 
-      for (let i = 0; i < originalBuffer.length; i++) {
-        const colorIndex = originalBuffer[i];
-        if (colorIndex === transparentIndex) {
-          buffer.set([0, 0, 0, 0], i * 4);
-        } else {
-          const color = colors[colorIndex];
-          buffer.set([color.r, color.g, color.b, 255], i * 4);
+    const frames = await Promise.all(
+      ase.frames.map(async (frame) => {
+        const cel = frame.chunks.find((c) => c.type === ChunkTypeEnum.CEL);
+        if (!cel) throw new Error(`missing cel chunk in ${entrypoint}`);
+        if (cel.data.type !== CelTypeEnum.COMPRESSED)
+          throw new Error(
+            `unsupported cel type in ${entrypoint}: ${cel.data.type}`,
+          );
+
+        let buffer: Buffer | Uint8Array = cel.data.pixels;
+        if (mode === ColorModeEnum.INDEXED) {
+          const originalBuffer = buffer;
+          buffer = new Uint8Array(originalBuffer.length * 4);
+
+          const transparentIndex = ase.header.transparentIndex;
+          const colors = palette.data.packets[0].colors;
+
+          for (let i = 0; i < originalBuffer.length; i++) {
+            const colorIndex = originalBuffer[i];
+            if (colorIndex === transparentIndex) {
+              buffer.set([0, 0, 0, 0], i * 4);
+            } else {
+              const color = colors[colorIndex];
+              buffer.set([color.r, color.g, color.b, 255], i * 4);
+            }
+          }
         }
-      }
-    }
 
-    const image = await sharp(buffer, {
-      raw: {
-        width: cel.data.width,
-        height: cel.data.height,
-        channels: mode === ColorModeEnum.INDEXED ? 4 : mode,
-      },
-    });
-    const { info } = await image.toBuffer({ resolveWithObject: true });
-    image
-      .extend({
-        top: cel.data.y,
-        left: cel.data.x,
-        bottom: ase.header.height - info.height - cel.data.y,
-        right: ase.header.width - info.width - cel.data.x,
-        background: { r: 0, g: 0, b: 0, alpha: 0 },
-      })
-      .png()
-      .toFile(`${output}/${baseName}.png`);
+        const image = await sharp(buffer, {
+          raw: {
+            width: cel.data.width,
+            height: cel.data.height,
+            channels: mode === ColorModeEnum.INDEXED ? 4 : mode,
+          },
+        });
+        const { info } = await image.toBuffer({ resolveWithObject: true });
+        return image.extend({
+          top: cel.data.y,
+          left: cel.data.x,
+          bottom: ase.header.height - info.height - cel.data.y,
+          right: ase.header.width - info.width - cel.data.x,
+          background: { r: 0, g: 0, b: 0, alpha: 0 },
+        });
+      }),
+    );
+
+    if (frames.length === 1) {
+      // output single frame
+      frames[0].png().toFile(`${output}/${baseName}.png`);
+    } else {
+      // create basic side-by-side spritesheet
+      const base = await sharp({
+        create: {
+          width: ase.header.width * ase.header.numFrames,
+          height: ase.header.height,
+          channels: 4,
+          background: { r: 0, g: 0, b: 0, alpha: 0 },
+        },
+      });
+
+      const inputs = await Promise.all(
+        frames.map(async (frame, i) => ({
+          input: await frame.png().toBuffer(),
+          top: 0,
+          left: i * ase.header.width,
+        })),
+      );
+      base.composite(inputs).png().toFile(`${output}/${baseName}.png`);
+    }
   }
 }
