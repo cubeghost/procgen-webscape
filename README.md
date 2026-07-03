03e3e62e7643ed66812373f21f331049655b67fb
alex baldwin <ghostbaldwin@gmail.com>
2026-07-03 00:58:16 -0400

    counter, font, icons

---
+++ README.md
@@ -0,0 +1,5 @@
+### Licenses
+
+All code falls under the [MIT License](https://opensource.org/license/mit).
+
+`/src/assets/cat.aseprite` is a derivative of [piixl's 1-Bit Icons Part-1](https://piiixl.itch.io/1-bit-16px-icons-part-1), which is [CC BY-ND 4.0](https://creativecommons.org/licenses/by-nd/4.0/deed.en).
---
+++ netlify/functions/counter.ts
@@ -0,0 +1,60 @@
+import { Context } from "@netlify/functions";
+import { Redis } from "@upstash/redis";
+import { isbot } from "isbot";
+
+const redis = Redis.fromEnv();
+
+export default async function (request: Request, context: Context) {
+  if (isbot(request.headers.get("User-Agent"))) {
+    return new Response("<!DOCTYPE html>", {
+      headers: {
+        "Content-Type": "text/html",
+        "Content-Security-Policy": "frame-ancestors 'self'",
+        "X-Frame-Options": "SAMEORIGIN",
+      },
+    });
+  }
+
+  const count = await redis.incr("count");
+  const digits = count.toString().split("");
+
+  return new Response(
+    `<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+    <link rel="stylesheet" href="/styles/counter.css">
+  </head>
+  <body>
+    <div class="visitors-container window window-utility window-utility-side">
+      <div class="window-handle">
+        <span class="window-button button-top"></span>
+        <span class="window-button button-bottom button-minimize"></span>
+      </div>
+      <div class="window-contents">
+        <img src="/assets/icons-16.png" alt="sheep" class="sprite-16" style="object-position: -48px 0">
+        <div class="visitors">
+          <div class="visitors-count">
+            ${digits
+              .map(
+                (digit) =>
+                  `<span class="visitors-number" data-number="${digit}">${digit}</span>`,
+              )
+              .join("\n")}
+          </div>
+          <span class="visitors-label"> visitors</span>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+`,
+    {
+      headers: {
+        "Content-Type": "text/html",
+        "Content-Security-Policy": "frame-ancestors 'self'",
+        "X-Frame-Options": "SAMEORIGIN",
+      },
+    },
+  );
+}
