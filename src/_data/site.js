70e07d7af599c00762a879d98ee3456612f1efa7
alex baldwin <ghostbaldwin@gmail.com>
2026-07-06 23:54:33 -0400

    get base url from deploy

---
+++ src/_data/site.js
@@ -0,0 +1,10 @@
+export default function () {
+  return {
+    url:
+      process.env.CONTEXT === "production"
+        ? process.env.URL
+        : process.env.CONTEXT === "dev"
+          ? `http://localhost:${process.env.PORT ?? 8888}/`
+          : (process.env.DEPLOY_PRIME_URL ?? process.env.URL),
+  };
+}
