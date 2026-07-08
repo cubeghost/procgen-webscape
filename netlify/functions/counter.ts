d11b331326158d9b125b0fd005010b7e62bcfbb3
alex baldwin <ghostbaldwin@gmail.com>
2026-07-08 18:15:47 -0400

    click counter

--- netlify/functions/counter.ts
+++ netlify/functions/counter.ts
@@ -15,8 +15,17 @@ export default async function (request: Request, context: Context) {
     });
   }
 
-  const count = await redis.incr("count");
+  const count =
+    request.method === "POST"
+      ? await redis.incr("count")
+      : ((await redis.get<number>("count")) ?? 0);
   const digits = count.toString().split("");
+
+  if (request.headers.get("Accept") === "application/json") {
+    return Response.json({
+      digits,
+    });
+  }
 
   return new Response(
     `<!DOCTYPE html>
@@ -31,21 +40,36 @@ export default async function (request: Request, context: Context) {
         <span class="window-button button-top"></span>
         <span class="window-button button-bottom button-minimize"></span>
       </div>
-      <div class="window-contents">
-        <img src="/assets/icons-16.png" alt="sheep" class="sprite-16" style="object-position: -48px 0">
-        <div class="visitors">
-          <div class="visitors-count">
-            ${digits
-              .map(
-                (digit) =>
-                  `<span class="visitors-number" data-number="${digit}">${digit}</span>`,
-              )
-              .join("\n")}
+      <form class="window-contents" method="POST">
+        <button type="submit">
+          <img src="/assets/icons-16.png" alt="sheep" class="sprite-16" style="object-position: -48px 0">
+          <div class="visitors">
+            <div class="visitors-count" style="--num-digits: ${digits.length}">
+              ${digits
+                .map(
+                  (digit) =>
+                    `<span class="visitors-number" data-number="${digit}">${digit}</span>`,
+                )
+                .join("\n")}
+            </div>
+            <span class="visitors-label"> click me</span>
           </div>
-          <span class="visitors-label"> visitors</span>
-        </div>
-      </div>
+        </button>
+      </form>
     </div>
+    <script type="text/javascript">
+      document.querySelector("form").addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const response = await fetch("", {
+          method: "POST",
+          headers: { "Accept": "application/json" }
+        });
+        const { digits } = await response.json();
+        const count = document.querySelector(".visitors-count");
+        count.innerHTML = digits.map((digit) => \`<span class="visitors-number" data-number="\${digit}">\${digit}</span>\`).join("\\n");
+        count.style.setProperty("--num-digits", digits.length);
+      });
+    </script>
   </body>
 </html>
 `,
