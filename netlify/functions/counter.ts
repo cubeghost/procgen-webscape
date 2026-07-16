0783caa6008d89cc09a824ea8cc081a7f36d6ad4
alex baldwin <ghostbaldwin@gmail.com>
2026-07-15 22:22:24 -0400

    counter cleanup, some a11y

--- netlify/functions/counter.ts
+++ netlify/functions/counter.ts
@@ -1,10 +1,9 @@
-import { Context } from "@netlify/functions";
 import { Redis } from "@upstash/redis";
 import { isbot } from "isbot";
 
 const redis = Redis.fromEnv();
 
-export default async function (request: Request, context: Context) {
+export default async function (request: Request) {
   if (isbot(request.headers.get("User-Agent"))) {
     return new Response("<!DOCTYPE html>", {
       headers: {
@@ -28,17 +27,26 @@ export default async function (request: Request, context: Context) {
   }
 
   const align = new URL(request.url).searchParams.get("align");
-  const bodyClass = align === "left" ? "align-left" : "align-right";
 
-  return new Response(
-    `<!DOCTYPE html>
+  return new Response(counterTemplate(digits, align), {
+    headers: {
+      "Content-Type": "text/html",
+      "Content-Security-Policy": "frame-ancestors 'self'",
+      "X-Frame-Options": "SAMEORIGIN",
+    },
+  });
+}
+
+function counterTemplate(digits: string[], align: string | null) {
+  const bodyClass = align === "left" ? "align-left" : "align-right";
+  return `<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
     <link rel="stylesheet" href="/styles/counter.css">
   </head>
   <body class="${bodyClass}">
-    <div class="visitors-container window window-utility window-utility-side">
+    <div class="counter-container window window-utility window-utility-side">
       <div class="window-handle">
         <span class="window-button button-top"></span>
         <span class="window-button button-bottom button-minimize"></span>
@@ -47,15 +55,15 @@ export default async function (request: Request, context: Context) {
         <button type="submit">
           <img src="/assets/icons-16.png" alt="sheep" class="sprite-16" style="object-position: -48px 0">
           <div class="visitors">
-            <div class="visitors-count" style="--num-digits: ${digits.length}">
+            <div class="counter-count" style="--num-digits: ${digits.length}">
               ${digits
                 .map(
                   (digit) =>
-                    `<span class="visitors-number" data-number="${digit}">${digit}</span>`,
+                    `<span class="counter-number" data-number="${digit}">${digit}</span>`,
                 )
                 .join("\n")}
             </div>
-            <span class="visitors-label"> click me</span>
+            <span class="counter-label"> click me</span>
           </div>
         </button>
       </form>
@@ -68,20 +76,12 @@ export default async function (request: Request, context: Context) {
           headers: { "Accept": "application/json" }
         });
         const { digits } = await response.json();
-        const count = document.querySelector(".visitors-count");
-        count.innerHTML = digits.map((digit) => \`<span class="visitors-number" data-number="\${digit}">\${digit}</span>\`).join("\\n");
+        const count = document.querySelector(".counter-count");
+        count.innerHTML = digits.map((digit) => \`<span class="counter-number" data-number="\${digit}">\${digit}</span>\`).join("\\n");
         count.style.setProperty("--num-digits", digits.length);
       });
     </script>
   </body>
 </html>
-`,
-    {
-      headers: {
-        "Content-Type": "text/html",
-        "Content-Security-Policy": "frame-ancestors 'self'",
-        "X-Frame-Options": "SAMEORIGIN",
-      },
-    },
-  );
+`;
 }
--- src/code/deep-web-transmissions.md
+++ src/code/deep-web-transmissions.md
@@ -7,5 +7,5 @@ date: 2020-05-11
 url: https://deep-web-transmissions.netlify.app/
 preview:
   src: /code/deep-web-transmissions.png
-  alt: TODO
+  alt: "screenshot of a website. on the left are two panels: the first has several form fields under the headings 'from', 'content', and 'printer'. the 'content' fields include a custom message and a reorderable set of checkboxes labeled with the names of generative art bots. the 'printer' section has an input to connect to a printer, buttons to check the connection and print, and a checkbox to preview printer dithering. the second panel is titled 'what is this?' and reads 'deep web transmissions is a tribute to generative art bots, formatted as a digest for the little printer and compatible thermal printers'. on the right of the page is a preview of a receipt, which contains the heading 'deep web transmissions', a tiny star field made of unicode characters, and a tarot card from uncle buddy's phantom funhouse."
 ---
